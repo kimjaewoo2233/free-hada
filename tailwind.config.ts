920c5a749ce234aa43074c1344b0c17ef3becd3b
kimjaewoo2233 <79129475+kimjaewoo2233@users.noreply.github.com>
2024-08-24 21:29:51 +0900

    config: 기본 폰트 및 컬러설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,52 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: { //메인컬러
+          blue: "#2A43C4",
+          yellow: "#2A43C4"
+        },  
+        neutral: { //네츄럴 컬러
+          100: "#FFFFFF",
+          98: "#F6F7F8",
+          80: "#D3D5DA",
+          70: "#BABDC5",
+          60: "#80848E",
+          40: "#606571",
+          25: "#33353D",
+          20: "#2A2B32",
+          10: "#17181C",
+          0: "#0A0A0A"
+        },
+        fontColor: {
+          100: '#222222', 
+          200: '#333333', 
+          300: '#505050', // 사용 예: text-fontColor-300
+        },
+        lineColor:{
+          100: "#05070F",
+          90: "#05070F"
+        }
       },
+      fontSize: {
+        xs: ['0.75rem', { lineHeight: '1rem' }],
+        sm: ['0.875rem', { lineHeight: '1.25rem' }],
+        base: ['1rem', { lineHeight: '1.5rem' }],
+        lg: ['1.125rem', { lineHeight: '1.75rem' }],
+        xl: ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+        '5xl': ['3rem', { lineHeight: '1' }],
+        '6xl': ['3.75rem', { lineHeight: '1' }],
+        '7xl': ['4.5rem', { lineHeight: '1' }],
+        '8xl': ['6rem', { lineHeight: '1' }],
+        '9xl': ['8rem', { lineHeight: '1' }],
+      },
+      fontFamily: {
+        sans: ['Pretendard Variable', 'sans-serif'], // font-sans
+        sticking: ['StickingCloseFont', 'sans-serif'],
+      }
     },
   },
   plugins: [],
